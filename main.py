5f99644850b7c197af250ad732326450a8e68bfe
Uwa Agbai <98921828+uwaagbai1@users.noreply.github.com>
2022-06-16 13:29:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+uwa_move = turtle.Turtle()
+def square():
+ uwa_move.speed(10)
+ uwa_move.forward(100)
+ uwa_move.right(90)
+ uwa_move.forward(100)
+ uwa_move.right(90)
+ uwa_move.forward(100)
+ uwa_move.right(90)
+ uwa_move.forward(100)
+ uwa_move.right(90)
+ 
+
+#square()
+for i in range(1):
+  square()
+  uwa_move.forward(200)
+  square()
